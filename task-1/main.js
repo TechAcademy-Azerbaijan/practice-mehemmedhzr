1ba0cb76a36bbf6524e5ff868cc82e075840c15d
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-05 01:18:15 -0700

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,9 @@
 const prompt = require('prompt');
 prompt.start();
 
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+prompt.get('input', function (err, result) { 
+    let number =parseInt(result.input)
+    console.log(parseInt(number/100)*100 + parseInt((number%100)/20)*30 + ((number%100)%20)*2);
+    
 });
+    
