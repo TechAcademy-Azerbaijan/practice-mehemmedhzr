4afbea5dcf1f63608f3c12b19a6a0ca789c7504d
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-05 01:08:47 -0700

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,26 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+const prompt = require('prompt');
+prompt.start();
+
+prompt.get('input', function (err, result) {
+let n = result.input.split(',')
+let nth = 1;
+let b = n[1];
+for(let i = n[0]; i <= b; i++){
+    nth = nth*i
+    if(nth > 0){
+        res = 1
+    }
+    else if(nth < 0){
+        res = -1
+    }
+    else{
+        res = 0
+    }
+}
+console.log(res);
+})
   
 });
