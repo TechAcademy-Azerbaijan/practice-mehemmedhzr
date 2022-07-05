906d735b3c3e2bb8487366100df658c1dbb39c98
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-05 01:27:54 -0700

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,13 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let n = result.input.split(',');
+let a = n[0]
+let b = n[1]
+let c = n[2]
+let d = n[3]
+let arr = [a,b,c,d]
+let max = Math.max(...arr)
+console.log(max);
+})
