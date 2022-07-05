af8ca3c08befe66c76e51de7317fd72b8fda3831
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-05 01:11:21 -0700

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,13 +1,10 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-let arr = result.input.split(',')[0]
+let arr = result.input.split(',')
 let fixed = arr.reverse()
 for(let i = 0; i < arr.length; i++){
     console.log(fixed[i]);
 }
-  
-});
+})
