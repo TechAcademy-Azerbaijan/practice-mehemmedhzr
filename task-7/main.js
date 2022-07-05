631600ae107e0242b9f9e2c459c4f1d141984e40
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-05 00:34:53 -0700

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -1,9 +1,12 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let n = result.input
+let sqrt = Math.sqrt(n)
+for(let i = 1; i < n; i++){
+    if(i**2 < n){
+        console.log(i**2);
+    }
+}
+})
