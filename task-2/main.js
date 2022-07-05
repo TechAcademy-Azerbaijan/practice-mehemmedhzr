44b3bc86044861a4ba50e2dbbff1d6266dac7583
mehemmedhzr <105041148+mehemmedhzr@users.noreply.github.com>
2022-07-05 00:05:22 -0700

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -3,7 +3,22 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  let n = result.input
+
+if (n === 1) {
+    res = "Yes";
+}
+
+else if (n > 1) {
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            res = "No";
+            break;
+        }
+        else if(n % i !== 0){
+            res = "Yes"
+        }
+    }
+}
+console.log(res);
 });
